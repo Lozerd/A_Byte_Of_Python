054cadb0770f04b7cafd82ca8c6c6240ca310753
Lozerd <dmisha29411@gmail.com>
2020-09-12 20:19:02 +0300

    inherit.py ver 1.01

--- Workspace/inherit.py
+++ Workspace/inherit.py
@@ -1,8 +1,9 @@
 class SchoolMember:
     """Представляет любого человека в школе."""
 
+    # __init__ - при передаче аргумента первый класс в кортеже принимает эти аргументы в функцию
     def __init__(self, name, age):
-        self.name = name
+        self.name = name  # присваивание self.name аргумента
         self.age = age
 
         print('(Создан новый SchoolMember: {0})'.format(self.name))
@@ -12,16 +13,16 @@ class SchoolMember:
         print('Имя: {0} Возраст: "{1}"'.format(self.name, self.age), end=" ")
 
 
-class Teacher(SchoolMember):
+class Teacher(SchoolMember):  # передача SchoolMember как аргумента для наследственности
     """Представляет преподавателя"""
 
     def __init__(self, name, age, salary):
-        SchoolMember.__init__(self, name, age)
-        self.salary = salary
+        SchoolMember.__init__(self, name, age)  # используя наследственность, передаём инициализированные аргументы
+        self.salary = salary  # передача дополнительной перменной
         print('(Создан новый Teacher: {0})'.format(self.name))
 
     def tell(self):
-        SchoolMember.tell(self)
+        SchoolMember.tell(self)  # использование SchoolMember как "аргумента"
         print('Зарплата: "{0}"'.format(self.salary))
 
 
@@ -30,7 +31,7 @@ class Student(SchoolMember):
 
     def __init__(self, name, age, marks):
         SchoolMember.__init__(self, name, age)
-        self.marks = marks
+        self.marks = marks  # ещё один доп аргумент (зарплата)
         print('(Создан новый Student: {0})'.format(self.name))
 
     def tell(self):
@@ -41,7 +42,7 @@ class Student(SchoolMember):
 t = Teacher('Mrs. Shrividya', 40, 30000)
 s = Student('Swaroop', 25, 75)
 
-print()
+print()  # пустое место
 
 members = [t, s]
 for member in members:
